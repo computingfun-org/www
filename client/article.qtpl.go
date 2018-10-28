884ebb5248fd670ed47df394c44a24a4facd0912
Zachary Cohen <zachary@computingfun.org>
2018-10-28 13:38:35 -0400

    html bugfixs and stripspace

--- client/article.qtpl.go
+++ client/article.qtpl.go
@@ -23,133 +23,115 @@ var (
 //line article.qtpl:3
 type ArticlePage articles.Article
 
-//line article.qtpl:5
+//line article.qtpl:7
 func (a ArticlePage) streamhead(qw422016 *qt422016.Writer) {
-	//line article.qtpl:5
-	qw422016.N().S(`
-<title><%=s a.Title %> - Computing Fun</title>
-<meta name="description" content="<%=s a.Details %>">
-<meta name="author" content="">
-<link rel="stylesheet" type="text/css" href="/client/pages/articles.css">
-<link rel="stylesheet" type="text/css" href="/client/base/prettyprint.css">
-<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-`)
-//line article.qtpl:12
-}
-
-//line article.qtpl:12
-func (a ArticlePage) writehead(qq422016 qtio422016.Writer) {
-	//line article.qtpl:12
-	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line article.qtpl:12
-	a.streamhead(qw422016)
-	//line article.qtpl:12
-	qt422016.ReleaseWriter(qw422016)
-//line article.qtpl:12
-}
-
-//line article.qtpl:12
-func (a ArticlePage) head() string {
-	//line article.qtpl:12
-	qb422016 := qt422016.AcquireByteBuffer()
-	//line article.qtpl:12
-	a.writehead(qb422016)
-	//line article.qtpl:12
-	qs422016 := string(qb422016.B)
-	//line article.qtpl:12
-	qt422016.ReleaseByteBuffer(qb422016)
-	//line article.qtpl:12
-	return qs422016
-//line article.qtpl:12
+	//line article.qtpl:7
+	qw422016.N().S(`<title>`)
+	//line article.qtpl:8
+	qw422016.E().S(a.Title)
+	//line article.qtpl:8
+	qw422016.N().S(`- Computing Fun</title><meta name="description" content="<%=s a.Details %>"><meta name="author" content=""><link rel="stylesheet" type="text/css" href="/client/pages/articles.css"><link rel="stylesheet" type="text/css" href="/client/base/prettyprint.css"><script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>`)
+//line article.qtpl:14
 }
 
 //line article.qtpl:14
-func (a ArticlePage) streambody(qw422016 *qt422016.Writer) {
+func (a ArticlePage) writehead(qq422016 qtio422016.Writer) {
 	//line article.qtpl:14
-	qw422016.N().S(`
-<article class="article">
-    <header class="title">
-        <h1 class="title-main">
-            `)
-	//line article.qtpl:18
+	qw422016 := qt422016.AcquireWriter(qq422016)
+	//line article.qtpl:14
+	a.streamhead(qw422016)
+	//line article.qtpl:14
+	qt422016.ReleaseWriter(qw422016)
+//line article.qtpl:14
+}
+
+//line article.qtpl:14
+func (a ArticlePage) head() string {
+	//line article.qtpl:14
+	qb422016 := qt422016.AcquireByteBuffer()
+	//line article.qtpl:14
+	a.writehead(qb422016)
+	//line article.qtpl:14
+	qs422016 := string(qb422016.B)
+	//line article.qtpl:14
+	qt422016.ReleaseByteBuffer(qb422016)
+	//line article.qtpl:14
+	return qs422016
+//line article.qtpl:14
+}
+
+//line article.qtpl:16
+func (a ArticlePage) streambody(qw422016 *qt422016.Writer) {
+	//line article.qtpl:16
+	qw422016.N().S(`<article class="article"><header class="title"><h1 class="title-main">`)
+	//line article.qtpl:20
 	qw422016.E().S(a.Title)
-	//line article.qtpl:18
-	qw422016.N().S(`
-        </h1>
-        <h2 class="title-sub">
-            `)
-	//line article.qtpl:21
+	//line article.qtpl:20
+	qw422016.N().S(`</h1><h2 class="title-sub">`)
+	//line article.qtpl:23
 	qw422016.E().S(a.Details)
-	//line article.qtpl:21
-	qw422016.N().S(`
-        </h2>
-        <div style="display: none;" class="info"><address><a rel="author" href="#"></a></address><time pubdate datetime=""></time></div>
-    </header>
-    <div class="article-content">
-        `)
-	//line article.qtpl:26
-	qw422016.E().S(a.Content)
-	//line article.qtpl:26
-	qw422016.N().S(`
-    </div>
-</article>
-`)
-//line article.qtpl:29
+	//line article.qtpl:23
+	qw422016.N().S(`</h2><div style="display: none;" class="info"><address><a rel="author" href="#"></a></address><time pubdate datetime=""></time></div></header><div class="article-content">`)
+	//line article.qtpl:28
+	qw422016.N().S(a.Content)
+	//line article.qtpl:28
+	qw422016.N().S(`</div></article>`)
+//line article.qtpl:31
 }
 
-//line article.qtpl:29
+//line article.qtpl:31
 func (a ArticlePage) writebody(qq422016 qtio422016.Writer) {
-	//line article.qtpl:29
+	//line article.qtpl:31
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line article.qtpl:29
+	//line article.qtpl:31
 	a.streambody(qw422016)
-	//line article.qtpl:29
+	//line article.qtpl:31
 	qt422016.ReleaseWriter(qw422016)
-//line article.qtpl:29
+//line article.qtpl:31
 }
 
-//line article.qtpl:29
+//line article.qtpl:31
 func (a ArticlePage) body() string {
-	//line article.qtpl:29
+	//line article.qtpl:31
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line article.qtpl:29
+	//line article.qtpl:31
 	a.writebody(qb422016)
-	//line article.qtpl:29
+	//line article.qtpl:31
 	qs422016 := string(qb422016.B)
-	//line article.qtpl:29
+	//line article.qtpl:31
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line article.qtpl:29
+	//line article.qtpl:31
 	return qs422016
-//line article.qtpl:29
+//line article.qtpl:31
 }
 
-//line article.qtpl:31
+//line article.qtpl:33
 func (a ArticlePage) streamnavLinks(qw422016 *qt422016.Writer) {
-//line article.qtpl:31
+//line article.qtpl:33
 }
 
-//line article.qtpl:31
+//line article.qtpl:33
 func (a ArticlePage) writenavLinks(qq422016 qtio422016.Writer) {
-	//line article.qtpl:31
+	//line article.qtpl:33
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line article.qtpl:31
+	//line article.qtpl:33
 	a.streamnavLinks(qw422016)
-	//line article.qtpl:31
+	//line article.qtpl:33
 	qt422016.ReleaseWriter(qw422016)
-//line article.qtpl:31
+//line article.qtpl:33
 }
 
-//line article.qtpl:31
+//line article.qtpl:33
 func (a ArticlePage) navLinks() string {
-	//line article.qtpl:31
+	//line article.qtpl:33
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line article.qtpl:31
+	//line article.qtpl:33
 	a.writenavLinks(qb422016)
-	//line article.qtpl:31
+	//line article.qtpl:33
 	qs422016 := string(qb422016.B)
-	//line article.qtpl:31
+	//line article.qtpl:33
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line article.qtpl:31
+	//line article.qtpl:33
 	return qs422016
-//line article.qtpl:31
+//line article.qtpl:33
 }
--- client/index.qtpl.go
+++ client/index.qtpl.go
@@ -20,112 +20,99 @@ var (
 //line index.qtpl:1
 type IndexPage struct{}
 
-//line index.qtpl:3
+//line index.qtpl:5
 func (i IndexPage) streamhead(qw422016 *qt422016.Writer) {
-	//line index.qtpl:3
-	qw422016.N().S(`
-<title>Computing Fun</title>
-<meta name="description" content="We build, teach and deploy all types of software solutions. From teaching non tech to developers. Building software and web servers of small businesses.">
-<link rel="stylesheet" type="text/css" href="/client/pages/index.css">
-`)
-//line index.qtpl:7
-}
-
-//line index.qtpl:7
-func (i IndexPage) writehead(qq422016 qtio422016.Writer) {
-	//line index.qtpl:7
-	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line index.qtpl:7
-	i.streamhead(qw422016)
-	//line index.qtpl:7
-	qt422016.ReleaseWriter(qw422016)
-//line index.qtpl:7
-}
-
-//line index.qtpl:7
-func (i IndexPage) head() string {
-	//line index.qtpl:7
-	qb422016 := qt422016.AcquireByteBuffer()
-	//line index.qtpl:7
-	i.writehead(qb422016)
-	//line index.qtpl:7
-	qs422016 := string(qb422016.B)
-	//line index.qtpl:7
-	qt422016.ReleaseByteBuffer(qb422016)
-	//line index.qtpl:7
-	return qs422016
-//line index.qtpl:7
+	//line index.qtpl:5
+	qw422016.N().S(`<title>Computing Fun</title><meta name="description" content="We build, teach and deploy all types of software solutions. From teaching non tech to developers. Building software and web servers of small businesses."><link rel="stylesheet" type="text/css" href="/client/pages/index.css">`)
+//line index.qtpl:9
 }
 
 //line index.qtpl:9
-func (i IndexPage) streambody(qw422016 *qt422016.Writer) {
+func (i IndexPage) writehead(qq422016 qtio422016.Writer) {
 	//line index.qtpl:9
-	qw422016.N().S(`
-<div class="nav-boxes">
-    <a style="background-color: #ff0000;" href="https://www.youtube.com/channel/UCeZQbACMihORscFIwmydpzA"><i class="fab fa-youtube"></i></a>
-    <a style="background-color: #f85944;" href="https://www.patreon.com/computingfun"><i class="fab fa-patreon"></i></a>
-    <a style="background-color: #171A21;" href="https://computingfun.org/games/"><i class="fas fa-gamepad"></i></a>
-    <a style="background-color: #77ab59;" href="https://computingfun.org/articles/"><i class="far fa-newspaper"></i></a>
-    <a style="background-color: #3B5998;" href="https://www.facebook.com/ComputingFun/"><i class="fab fa-facebook-f"></i></a>
-    <a style="background-color: #6441A4;" href="#"><i class="fab fa-twitch"></i></a>
-</div>
-`)
-//line index.qtpl:18
+	qw422016 := qt422016.AcquireWriter(qq422016)
+	//line index.qtpl:9
+	i.streamhead(qw422016)
+	//line index.qtpl:9
+	qt422016.ReleaseWriter(qw422016)
+//line index.qtpl:9
 }
 
-//line index.qtpl:18
+//line index.qtpl:9
+func (i IndexPage) head() string {
+	//line index.qtpl:9
+	qb422016 := qt422016.AcquireByteBuffer()
+	//line index.qtpl:9
+	i.writehead(qb422016)
+	//line index.qtpl:9
+	qs422016 := string(qb422016.B)
+	//line index.qtpl:9
+	qt422016.ReleaseByteBuffer(qb422016)
+	//line index.qtpl:9
+	return qs422016
+//line index.qtpl:9
+}
+
+//line index.qtpl:11
+func (i IndexPage) streambody(qw422016 *qt422016.Writer) {
+	//line index.qtpl:11
+	qw422016.N().S(`<div class="nav-boxes"><a style="background-color: #ff0000;" href="https://www.youtube.com/channel/UCeZQbACMihORscFIwmydpzA"><i class="fab fa-youtube"></i></a><a style="background-color: #f85944;" href="https://www.patreon.com/computingfun"><i class="fab fa-patreon"></i></a><a style="background-color: #171A21;" href="https://computingfun.org/games/"><i class="fas fa-gamepad"></i></a><a style="background-color: #77ab59;" href="https://computingfun.org/articles/"><i class="far fa-newspaper"></i></a><a style="background-color: #3B5998;" href="https://www.facebook.com/ComputingFun/"><i class="fab fa-facebook-f"></i></a><a style="background-color: #6441A4;" href="#"><i class="fab fa-twitch"></i></a></div>`)
+//line index.qtpl:20
+}
+
+//line index.qtpl:20
 func (i IndexPage) writebody(qq422016 qtio422016.Writer) {
-	//line index.qtpl:18
+	//line index.qtpl:20
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line index.qtpl:18
+	//line index.qtpl:20
 	i.streambody(qw422016)
-	//line index.qtpl:18
+	//line index.qtpl:20
 	qt422016.ReleaseWriter(qw422016)
-//line index.qtpl:18
+//line index.qtpl:20
 }
 
-//line index.qtpl:18
+//line index.qtpl:20
 func (i IndexPage) body() string {
-	//line index.qtpl:18
+	//line index.qtpl:20
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line index.qtpl:18
+	//line index.qtpl:20
 	i.writebody(qb422016)
-	//line index.qtpl:18
+	//line index.qtpl:20
 	qs422016 := string(qb422016.B)
-	//line index.qtpl:18
+	//line index.qtpl:20
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line index.qtpl:18
+	//line index.qtpl:20
 	return qs422016
-//line index.qtpl:18
+//line index.qtpl:20
 }
 
-//line index.qtpl:20
+//line index.qtpl:22
 func (i IndexPage) streamnavLinks(qw422016 *qt422016.Writer) {
-//line index.qtpl:20
+//line index.qtpl:22
 }
 
-//line index.qtpl:20
+//line index.qtpl:22
 func (i IndexPage) writenavLinks(qq422016 qtio422016.Writer) {
-	//line index.qtpl:20
+	//line index.qtpl:22
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line index.qtpl:20
+	//line index.qtpl:22
 	i.streamnavLinks(qw422016)
-	//line index.qtpl:20
+	//line index.qtpl:22
 	qt422016.ReleaseWriter(qw422016)
-//line index.qtpl:20
+//line index.qtpl:22
 }
 
-//line index.qtpl:20
+//line index.qtpl:22
 func (i IndexPage) navLinks() string {
-	//line index.qtpl:20
+	//line index.qtpl:22
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line index.qtpl:20
+	//line index.qtpl:22
 	i.writenavLinks(qb422016)
-	//line index.qtpl:20
+	//line index.qtpl:22
 	qs422016 := string(qb422016.B)
-	//line index.qtpl:20
+	//line index.qtpl:22
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line index.qtpl:20
+	//line index.qtpl:22
 	return qs422016
-//line index.qtpl:20
+//line index.qtpl:22
 }
