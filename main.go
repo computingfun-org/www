8cb7a2426e78354d0779daa250638bd8b20ce397
Zachary Cohen <zachary@computingfun.org>
2018-12-24 19:42:51 -0500

    split router logic from main

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
 	"gitlab.com/computingfun/www/articles"
-	"gitlab.com/computingfun/www/client"
 	"gitlab.com/zacc/autocertcache"
 	"golang.org/x/crypto/acme/autocert"
 
@@ -34,22 +32,7 @@ func main() {
 	}
 	defer ArticleStore.Close()
 
-	hfs, err := client.NewHTTPFileSystem()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	handler := httprouter.New()
-	handler.GET("/", IndexHandler)
-	handler.GET("/articles/", UnavailableHandler)
-	handler.GET("/articles/:id", ArticleHandler)
-	handler.GET("/games/", UnavailableHandler)
-	handler.GET("/games/:id", UnavailableHandler)
-	handler.ServeFiles("/client/*filepath", hfs)
-	handler.NotFound = http.HandlerFunc(NotFoundHandler)
-	handler.PanicHandler = PanicHandler
-
-	certCache, err := autocertcache.NewSQL(db, "Certs")
+	certCache, err := autocertcache.NewSQLite(db, "Certs")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -58,11 +41,11 @@ func main() {
 	cert := autocert.Manager{
 		Cache:      certCache,
 		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("computingfun.org", "www.computingfun.org", "beta.computingfun.org"),
+		HostPolicy: autocert.HostWhitelist("www.computingfun.org"),
 	}
 
 	server := http.Server{
-		Handler:   handler,
+		Handler:   NewRouter(),
 		TLSConfig: cert.TLSConfig(),
 	}
 
@@ -73,39 +56,4 @@ func main() {
 
 	err = server.ListenAndServeTLS("", "")
 	log.Fatalln(err)
-}
-
-// UnavailableHandler is an adapter for ...
-func UnavailableHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.WriteHeader(http.StatusServiceUnavailable)
-	client.WriteHTML(w, client.UnavailablePage)
-}
-
-// NotFoundHandler is an adapter for ...
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	client.WriteHTML(w, client.NotFoundPage)
-}
-
-// PanicHandler is an adapter for ...
-func PanicHandler(w http.ResponseWriter, r *http.Request, e interface{}) {
-	go log.Println("Panic: ", e, " | Request: ", r)
-	w.WriteHeader(http.StatusInternalServerError)
-	client.WriteHTML(w, client.PanicPage)
-}
-
-// IndexHandler responses with the home page.
-func IndexHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	client.WriteHTML(w, client.IndexPage{})
-}
-
-// ArticleHandler responses with an article page for the article with id [:id].
-// If article is not found ArticleHandler responses with NotFoundHandler.
-func ArticleHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	a, err := ArticleStore.Get(p.ByName("id"))
-	if err != nil {
-		NotFoundHandler(w, r)
-		return
-	}
-	client.WriteHTML(w, client.ArticlePage(a))
 }
---
+++ router.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gitlab.com/computingfun/www/client"
+)
+
+// NewRouter ...
+func NewRouter() http.Handler {
+	h := httprouter.New()
+	h.GET("/", IndexHandler)
+	h.GET("/articles/", UnavailableHandler)
+	h.GET("/articles/:id", ArticleHandler)
+	h.GET("/games/", UnavailableHandler)
+	h.GET("/games/:id", UnavailableHandler)
+
+	hfs, err := client.NewHTTPFileSystem()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h.ServeFiles("/client/*filepath", hfs)
+
+	h.NotFound = http.HandlerFunc(NotFoundHandler)
+	h.PanicHandler = PanicHandler
+	return h
+}
+
+// UnavailableHandler is an adapter for ...
+func UnavailableHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusServiceUnavailable)
+	client.WriteHTML(w, client.UnavailablePage)
+}
+
+// NotFoundHandler is an adapter for ...
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	client.WriteHTML(w, client.NotFoundPage)
+}
+
+// PanicHandler is an adapter for ...
+func PanicHandler(w http.ResponseWriter, r *http.Request, e interface{}) {
+	go log.Println("Panic: ", e, " | Request: ", r)
+	w.WriteHeader(http.StatusInternalServerError)
+	client.WriteHTML(w, client.PanicPage)
+}
+
+// IndexHandler responses with the home page.
+func IndexHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	client.WriteHTML(w, client.IndexPage{})
+}
+
+// ArticleHandler responses with an article page for the article with id [:id].
+// If article is not found ArticleHandler responses with NotFoundHandler.
+func ArticleHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	a, err := ArticleStore.Get(p.ByName("id"))
+	if err != nil {
+		NotFoundHandler(w, r)
+		return
+	}
+	client.WriteHTML(w, client.ArticlePage(a))
+}
