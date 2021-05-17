ae39b48d3b76a65d582b75efc21b0f338c392f96
Zachary Cohen <zachary@computingfun.org>
2021-05-16 22:27:28 -0400

    start switch to gofiber

--- go.mod
+++ go.mod
@@ -3,12 +3,6 @@ module gitlab.com/computingfun/www
 go 1.12
 
 require (
-	cloud.google.com/go v0.37.1
-	firebase.google.com/go v3.6.0+incompatible
-	github.com/julienschmidt/httprouter v1.2.0
-	github.com/rakyll/statik v0.1.6
-	github.com/valyala/quicktemplate v1.4.1
-	gitlab.com/zacc/autocertcache v0.0.0-20190322021307-e372856959c8
-	golang.org/x/crypto v0.0.0-20190320223903-b7391e95e576
-	google.golang.org/api v0.2.0
+	github.com/gofiber/fiber/v2 v2.9.0
+	github.com/gofiber/template v1.6.9
 )
--- main.go
+++ main.go
@@ -1,84 +1,41 @@
 package main
 
-//go:generate go generate ./client
-
 import (
-	"context"
-	"flag"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"fmt"
 	"os"
-	"path/filepath"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/julienschmidt/httprouter"
-	"gitlab.com/computingfun/www/client"
-	"gitlab.com/computingfun/www/firestoredb"
-	"gitlab.com/zacc/autocertcache"
-	"golang.org/x/crypto/acme/autocert"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 )
 
-// InstallFlag is the systemd installer flag.
-var InstallFlag *bool
-
-func init() {
-	InstallFlag = flag.Bool("systemd", false, "Install systemd service 💾 ")
-}
+const port = ":8080"
 
 func main() {
-	flag.Parse()
 
-	// Handles installing systemd service if InstallFlag is set.
-	if *InstallFlag {
-		log.Println("💾  Installing systemd service:")
-		path, err := os.Executable()
-		if err != nil {
-			log.Println("\t❌  Failed: Unable to find path: " + err.Error())
-			os.Exit(1)
-		}
-		file := []byte("\n[Unit]\nDescription=Computing Fun web server\n[Service]\nExecStart=" + path + "\nWorkingDirectory=" + filepath.Dir(path) + "\n[Install]\nWantedBy=multi-user.target")
-		err = ioutil.WriteFile("/etc/systemd/system/cf-www.service", file, 0664)
-		if err != nil {
-			log.Println("\t❌  Failed: Unable to write file: " + err.Error())
-			os.Exit(1)
-		}
-		log.Println("\t✔️  Success")
-		os.Exit(0)
-	}
+	server := fiber.New(fiber.Config{
+		IdleTimeout:      time.Minute,
+		WriteTimeout:     time.Minute,
+		ReadTimeout:      time.Minute,
+		DisableKeepalive: true,
+		Views:            html.New("./views", ".html"),
+	})
 
-	router := httprouter.New()
-	router.GET("/", IndexHandler)
-	router.GET("/articles/", UnavailableHandler)
-	router.GET("/articles/:id", ArticleHandler)
-	router.GET("/games/", UnavailableHandler)
-	router.GET("/games/:id", UnavailableHandler)
-	router.ServeFiles("/client/*filepath", client.NewHTTPFileSystemLogErr())
-	router.NotFound = http.HandlerFunc(NotFoundHandler)
-	router.PanicHandler = PanicHandler
-
-	if err := firestoredb.Init(context.TODO(), "credentials.json"); err != nil {
-		log.Fatalln("🔑  " + err.Error())
-	}
-
-	cert := autocert.Manager{
-		Cache:      autocertcache.NewFirestore(firestoredb.AutoCertCache),
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("www.computingfun.org", "beta.computingfun.org"),
-		Email:      "security@computingfun.org",
-	}
-
-	server := http.Server{
-		Handler:   router,
-		TLSConfig: cert.TLSConfig(),
-	}
-
-	log.Println("🌐")
+	server.Get("/", func(c *fiber.Ctx) error {
+		return nil
+	})
 
 	go func() {
-		err := http.ListenAndServe("", cert.HTTPHandler(nil))
-		log.Fatalln(err)
+		if err := server.Listen(port); err != nil {
+			panic(err)
+		}
 	}()
+	defer fmt.Println("🛑 server shutdown - error:", server.Shutdown())
 
-	err := server.ListenAndServeTLS("", "")
-	log.Fatalln(err)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	s := <-signals
+	fmt.Println("🏁 signal:", s)
 }
